1f0214327ffc4be665f08f52197e5ee9c0242826
rustamSurmashev <rustamsurmashev@gmail.com>
2018-12-26 13:31:25 +0400

    Test modifided

--- test/run.test.js
+++ test/run.test.js
@@ -1,8 +1,7 @@
 import {calculateRun} from "../js/run";
 test ('should show run', () =>{
-    const rate = 8.5;
-    const volume = 100;
-    let result = Math.round(volume*100/rate);
 
-    expect(result).toBe(1176);
+    let result = calculateRun(10, 100);
+
+    expect(result).toBe(1000);
 });
