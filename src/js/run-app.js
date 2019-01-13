9e5bcfab64b57fcf573aa7770b510641ba8baa0c
rustamSurmashev <rustamsurmashev@gmail.com>
2019-01-13 17:02:45 +0400

    Some text added

--- src/js/run-app.js
+++ src/js/run-app.js
@@ -9,6 +9,31 @@ calculateEl.addEventListener('click', function () {
     const rate = parseInt(rateEl.value);
     const volume = parseInt(volumeEl.value);
 
+    if (isNaN(rate)) {
+        resultEl.textContent = 'Введите в поле РАСХОД число';
+        resultDescriptionEl.textContent = '';
+        return;
+    }
+
+    if (isNaN(volume)) {
+        resultEl.textContent = 'Введите в поле ОБЪЁМ число';
+        resultDescriptionEl.textContent = '';
+        return;
+    }
+
+    if (rate < 1) {
+        resultEl.textContent = 'Расход должен быть больше 1 литра на 100 км';
+        resultDescriptionEl.textContent = '';
+        return;
+    }
+
+    if (volume < 1) {
+        resultEl.textContent = 'Объём должен быть больше 1 литра';
+        resultDescriptionEl.textContent = '';
+        return;
+    }
+
+
     resultEl.textContent = calculateRun(rate, volume);
 
 });
